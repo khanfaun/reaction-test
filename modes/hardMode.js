3ce107300644ed6e5cb01103dcdb788e225831ec
khanfaun <khangphan1612@gmail.com>
2025-06-19 00:41:52 +0700

    Update hardMode.js

--- modes/hardMode.js
+++ modes/hardMode.js
@@ -1,20 +1,3 @@
-const hardColors = ['yellow', 'orange', 'purple', 'pink', 'cyan']
-
 export function prepareHardModeSequence() {
-  const count = 5 + Math.floor(Math.random() * 3)
-  return generateRandomColorSequence(hardColors, count, 800, 1800)
-}
-
-function generateRandomColorSequence(colors, count, minDelay, maxDelay) {
-  let sequence = Array.from({ length: count }, () => {
-    const color = colors[Math.floor(Math.random() * colors.length)]
-    const delay = Math.floor(Math.random() * (maxDelay - minDelay)) + minDelay
-    return { color, delay }
-  })
-  sequence.push({ color: 'green', delay: 0 })
-  sequence = sequence.sort(() => Math.random() - 0.5)
-  if (sequence[0].color === 'green') {
-    [sequence[0], sequence[1]] = [sequence[1], sequence[0]]
-  }
-  return sequence
+  return [] // không dùng nữa, sẽ xử lý riêng trong index.js
 }
